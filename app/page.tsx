5250df5caa2546092beff37d0f2fe3cd54930009
profTanish <profile.tanish@gmail.com>
2025-05-13 15:05:09 +0530

    Created product page without functionalityes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import EarringsPage from "@/components/categoryPage";
 export default function Home() {
 
   return (
-    <div className="mt-17 md:mt-39 w-screen  mb-20">
+    <div className="mt-18 md:mt-39 w-screen  mb-20">
       <div className="md:mt-0 lg:mx-38 md:mx-10 mx-4">
         {/* <LandingPage /> */}
         <EarringsPage/>
