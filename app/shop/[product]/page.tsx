1f55fa61da34266c8fdff4cf547df7930ff1b0db
profTanish <profile.tanish@gmail.com>
2025-05-25 19:48:52 +0530

    Started to create product page

--- app/shop/[product]/page.tsx
+++ app/shop/[product]/page.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
-import { Hind } from "next/font/google";
+import { Button } from "@/components/ui/button";
+// import { Hind } from "next/font/google";
 
-const oswald =Hind({
-  subsets: ["latin"],
-  weight: ["500"],
-  display: "swap",
-})
+// const oswald = Hind({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600"],
+//   display: "swap",
+// })
 
 export default function Home() {
 
   return (
-    <div className="mt-18 md:mt-36 w-screen  mb-20">
+    <div className="mt-18 md:mt-34 w-screen  mb-20">
       <div className="md:mt-0 lg:mx-2.5 md:mx-10 mx-3">
-        <div className="grid gap-1.5 grid-cols-11 grid-rows-5 "> 
+        <div className="grid gap-1.5 grid-cols-11 grid-rows-5 ">
           <div className="col-span-4 row-start-1 row-end-5 space-y-1.5">
             <Image src={"/product/ring.avif"} alt="product" className="" width={800} height={800} />
             <Image src={"/product/ring2.png"} alt="product" className="" width={800} height={800} />
@@ -24,22 +25,45 @@ export default function Home() {
             <Image src={"/product/ring4.avif"} alt="product" className="" width={800} height={800} />
             <Image src={"/product/ring5.avif"} alt="product" className="" width={800} height={800} />
           </div>
-          <div className={`${oswald.className} col-span-3 row-start-1 row-end-5 mx-10 `}>
+          <div className={` col-span-3 row-start-1 row-end-5 mx-10 `}>
             <div className="flex justify-between wrap-normal">
-              <div className="text-2xl ">The Classic Emerald Engagement Ring</div>
+              <div className="text-2xl font-semibold">The Classic Emerald Engagement Ring</div>
               <div className="mt-2 ml-6">
                 <Image
-                src={"/svg/heart.svg"}
-                height={27}
-                width={27}
-                alt="heart"
-                className="-translate-y-0.5"
+                  src={"/svg/heart.svg"}
+                  height={27}
+                  width={27}
+                  alt="heart"
+                  className="-translate-y-0.5"
                 >
                 </Image>
               </div>
             </div>
-            <div className="mt-2.5 text-xl">Starting at $900</div>
-            <div className="mt-2.5 text-md">Shape: </div>
+
+            <div>
+              <div className="mt-2.5  text-xl">Starting at $900</div>
+              <div className="mt-2.5 text-md ">Shape: </div>
+              <div className="h-10"></div>
+              <div className="mt-2.5 text-md ">Band width: </div>
+              <div className="h-10"></div>
+              <div className="mt-2.5 text-md ">Metal: </div>
+              <div className="h-10"></div>
+              <div className="mt-2.5 text-md font-medium">Band: </div>
+              <div className="h-10"></div>
+            </div>
+
+            <div className="">
+              <Button className=" mb-3 text-white rounded-none w-full pt-3 h-12 text-md" >Select your RAYA created diamond</Button>
+              <Button className="rounded-none bg-white border-1 border-black hover:bg-gray-100 text-black w-full pt-3 h-12 text-md" > <Image src={"/svg/cal.svg"} height={20} width={20} alt="calendar" className="ml-2 mb-1"/> Consult with a diamond expert online </Button>
+            </div>
+
+            <div className="mt-6">
+              <div className="-translate-x-1 flex text-[15px] font-light"><Image src={"/svg/dimand.svg"} height={8} width={30} alt="calendar" className=" icon scale-55 mr-0.5 -translate-y-0.5"/>RAYA created diamonds <span className="font-bold ml-2 text-sm mt-0.5"> &#9432;</span></div>
+              <div className=" flex text-[15px] font-light"><Image src={"/svg/calender.svg"} height={8} width={20} alt="calendar" className="icon scale-70 mr-2 -translate-y-0.5"/>Made-to-order. Ships by Tue, Jun 10</div>
+              <div className=" flex underline text-gray-500 text-[15px] font-light"><Image src={"/svg/tick.svg"} height={8} width={20} alt="calendar" className="icon scale-65 mr-2 -translate-y-0.5"/>Lifetime warranty and value guarantee</div>
+              <div className=" -translate-x-1 flex underline text-gray-500 text-[15px] font-light"><Image src={"/svg/truck.svg"} height={8} width={30} alt="calendar" className="icon scale-55 mr-0.5 -translate-y-0.5"/>Shipping policy</div>
+              <div className=" flex underline -translate-x-1 text-gray-500 text-[15px] font-light"><Image src={"/svg/restart.png"} height={8} width={30} alt="calendar" className="-translate-y-1 icon scale-50 "/>Return policy</div>
+            </div>
           </div>
         </div>
       </div>
--- components/categoryPage.tsx
+++ components/categoryPage.tsx
@@ -784,7 +784,7 @@ export default function EarringsPage() {
 
                   <div className="relative mb-2 bg-gray-50 aspect-[5/6] md:aspect-square overflow-hidden">
                     {product.bestSeller && (
-                      <span className={`absolute top-2 left-2 bg-white md:text-[10px] text-[8.5px] px-2 py-1 z-5  ${montserrat.className} `}>
+                      <span className={`absolute font-mono top-2 left-2 bg-white md:text-[10px] text-[8.5px] px-2 py-1 z-5  ${montserrat.className} `}>
                         BEST SELLING
                       </span>
                     )}
