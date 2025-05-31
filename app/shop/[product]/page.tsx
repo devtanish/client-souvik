14922041ead29c7c019a43bd82078cdae79e0b94
profTanish <profile.tanish@gmail.com>
2025-05-31 18:12:33 +0530

    added some dynimic functionility

--- app/shop/[product]/page.tsx
+++ app/shop/[product]/page.tsx
@@ -48,8 +48,8 @@ export default function Home({ params }: { params: Promise<{ product: string }> 
   return (
     <div className="mt-18 md:mt-36 w-full mb-20">
       <div className="md:mt-0 lg:mx-2.5 md:mx-6 mx-0">
-        <div className="grid gap-1.5 grid-cols-1 lg:grid-cols-11 ">
-          <div className="flex justify-center lg:hidden -mx-4 md:mx-0">
+        <div className="grid gap-1.5 grid-cols-1 md:grid-cols-3 lg:grid-cols-11 ">
+          <div className="flex justify-center lg:hidden col-space-2 -mx-4 md:mx-0">
             <Carousel className="w-full max-w-none md:max-w-xs flex lg:hidden">
               <CarouselContent className="border-2">
                 {productcat?.productImg.map((img, index) => (
@@ -57,7 +57,7 @@ export default function Home({ params }: { params: Promise<{ product: string }> 
                     <div className="">
                       <Card className="border-none">
                         <CardContent className=" border-none flex aspect-square items-center justify-center scale-115">
-                          <Image src={img || "/placeholder.svg"} alt="product" className="cursor-crosshair w-full h-full" width={800} height={800} /> 
+                          <Image src={img || "/placeholder.svg"} alt="product" className="cursor-crosshair w-full h-full" width={800} height={800} />
                         </CardContent>
                       </Card>
                     </div>
@@ -78,116 +78,118 @@ export default function Home({ params }: { params: Promise<{ product: string }> 
               />
             ))}
           </div>
-          <div className={` col-span-3 mx-4 sticky`}>
-            <div className="flex justify-between wrap-normal">
-              <div className="text-2xl font-semibold">{productcat?.title}</div>
-              <div className="mt-2 ml-6">
-                <Image src={"/svg/heart.svg"} height={27} width={27} alt="heart" className="-translate-y-0.5"></Image>
-              </div>
-            </div>
-
-            <div>
-              <div className="mt-0 font-bold  text-xl flex">
-                Starting at{" "}
-                <div className={`${oswald.className} text-lg ml-1 translate-y-0.5`}> ${productcat?.price}</div>
-              </div>
-              <div className="mt-2.5 text-md ">Shape: </div>
-              <div className=" h-10">
-                <div className="mx-3 -translate-y-4 relative">
-                  <button
-                    onClick={() => scrollCategories("left")}
-                    className="absolute -left-5 top-1/2 -translate-y-2/3 -translate-x-2 z-10 bg-white/80 rounded-full p-1 shadow-md opacity-15 lg:flex hidden"
-                    aria-label="Scroll left"
-                  >
-                    <ChevronLeft size={20} />
-                  </button>
-
-                  <div
-                    ref={categoryScrollRef}
-                    className="flex overflow-x-auto scrollbar-hide gap-1 px-0 pb-0 py-0 scroll-smooth -translate-x-3"
-                    style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
-                  >
-                    {productcat?.activeStones.map((category, index) => (
-                      <div
-                        key={index}
-                        className={cn(
-                          "flex-shrink-0 flex flex-col items-center cursor-pointer transition-all -translate-y-0.5",
-                          " sm:w-1/7 w-1/6",
-                          activeStone === category.name ? "opacity-100 " : "opacity-80 hover:opacity-100",
-                        )}
-                        onClick={() => {
-                          console.log(category.name)
-                          setActiveStone(category.name)
-                        }}
-                      >
-                        <div className={`relative h-20 w-20`}>
-                          <Image
-                            src={category.element || "/placeholder.svg"}
-                            alt={category.name}
-                            fill
-                            className={`  scale-40`}
-                          />
-                        </div>
-                        {activeStone === category.name && (
-                          <div className="border-1 w-7 border-black transition-transform duration-300 -translate-y-4.5"></div>
-                        )}
-                      </div>
-                    ))}
-                  </div>
-
-                  <button
-                    onClick={() => scrollCategories("right")}
-                    className="absolute -right-6 top-1/2 -translate-y-2/3 z-10 bg-white/80 rounded-full p-1 shadow-md opacity-15 lg:flex hidden"
-                    aria-label="Scroll right"
-                  >
-                    <ChevronRight size={20} />
-                  </button>
+          <div className={` col-span-3 mx-1 md:mx-4 sticky`}>
+            <div className="col-span-1">
+              <div className="flex justify-between wrap-normal">
+                <div className="text-2xl mt-5 md:mt-0 font-semibold">{productcat?.title}</div>
+                <div className="mt-2 ml-6">
+                  <Image src={"/svg/heart.svg"} height={27} width={27} alt="heart" className="-translate-y-0.5"></Image>
                 </div>
               </div>
-              <div className="mt-2.5 text-md ">Select Size </div>
-              <div className="h-10">
-                <div className="mx-3 -translate-y-4 relative">
-                  <div
-                    ref={categoryScrollRef}
-                    className="flex overflow-x-auto scrollbar-hide gap-1 px-0 pb-0 py-0 scroll-smooth -translate-x-3"
-                    style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
-                  >
-                    {productcat?.sizes.map((category, index) => (
-                      <div
-                        key={index}
-                        className={cn(
-                          "flex-shrink-0 h-15 flex flex-col items-center cursor-pointer transition-all -translate-y-0.5",
-                          " sm:w-1/7 w-1/6",
-                          activeSize === category ? "opacity-100 " : "opacity-80 hover:opacity-100",
-                        )}
-                        onClick={() => {
-                          console.log(category)
-                          setActiveSize(category)
-                        }}
-                      >
-                        <div className={`relative h-20 w-20 ${oswald.className} `}>
-                          <div className={`${oswald.className} border h-6 w-7  text-center mt-6 ml-[1.6rem]`}>
-                            {category}
+
+              <div>
+                <div className="mt-0 font-bold  text-xl flex">
+                  Starting at{" "}
+                  <div className={`${oswald.className} text-lg ml-1 translate-y-0.5`}> ${productcat?.price}</div>
+                </div>
+                <div className="mt-2.5 text-md ">Shape: </div>
+                <div className=" h-10">
+                  <div className="mx-3 -translate-y-4 relative">
+                    <button
+                      onClick={() => scrollCategories("left")}
+                      className="absolute -left-5 top-1/2 -translate-y-2/3 -translate-x-2 z-10 bg-white/80 rounded-full p-1 shadow-md opacity-15 lg:flex hidden"
+                      aria-label="Scroll left"
+                    >
+                      <ChevronLeft size={20} />
+                    </button>
+
+                    <div
+                      ref={categoryScrollRef}
+                      className="flex overflow-x-auto scrollbar-hide gap-1 px-0 pb-0 py-0 scroll-smooth -translate-x-3"
+                      style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+                    >
+                      {productcat?.activeStones.map((category, index) => (
+                        <div
+                          key={index}
+                          className={cn(
+                            "flex-shrink-0 flex flex-col items-center cursor-pointer transition-all -translate-y-0.5",
+                            "lg:w-1/6 sm:w-1/10 w-1/6",
+                            activeStone === category.name ? "opacity-100 " : "opacity-80 hover:opacity-100",
+                          )}
+                          onClick={() => {
+                            console.log(category.name)
+                            setActiveStone(category.name)
+                          }}
+                        >
+                          <div className={`relative h-20 w-20`}>
+                            <Image
+                              src={category.element || "/placeholder.svg"}
+                              alt={category.name}
+                              fill
+                              className={`  scale-40`}
+                            />
                           </div>
+                          {activeStone === category.name && (
+                            <div className="border-1 w-7 border-black transition-transform duration-300 -translate-y-4.5"></div>
+                          )}
                         </div>
-                        {activeSize === category && (
-                          <div className="border-1 w-7 border-black transition-transform duration-300"></div>
-                        )}
-                      </div>
-                    ))}
+                      ))}
+                    </div>
+
+                    <button
+                      onClick={() => scrollCategories("right")}
+                      className="absolute -right-6 top-1/2 -translate-y-2/3 z-10 bg-white/80 rounded-full p-1 shadow-md opacity-15 lg:flex hidden"
+                      aria-label="Scroll right"
+                    >
+                      <ChevronRight size={20} />
+                    </button>
                   </div>
-                  <button
-                    className="underline cursor-pointer -translate-x-2.5 hover:text-gray-600 transition-colors"
-                    onClick={() => setIsSizeGuideOpen(true)}
-                  >
-                    size guide
-                  </button>
                 </div>
+                <div className="mt-2.5 text-md ">Select Size </div>
+                <div className="h-10">
+                  <div className="mx-3 -translate-y-4 relative">
+                    <div
+                      ref={categoryScrollRef}
+                      className="flex overflow-x-auto scrollbar-hide gap-1 px-0 pb-0 py-0 scroll-smooth -translate-x-3"
+                      style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+                    >
+                      {productcat?.sizes.map((category, index) => (
+                        <div
+                          key={index}
+                          className={cn(
+                            "flex-shrink-0 h-15 flex flex-col items-center cursor-pointer transition-all -translate-y-0.5",
+                            " lg:w-1/6 sm:w-1/10 w-1/6",
+                            activeSize === category ? "opacity-100 " : "opacity-80 hover:opacity-100",
+                          )}
+                          onClick={() => {
+                            console.log(category)
+                            setActiveSize(category)
+                          }}
+                        >
+                          <div className={`relative h-20 w-20 ${oswald.className} `}>
+                            <div className={`${oswald.className} border h-6 w-7  text-center mt-6 ml-[1.6rem]`}>
+                              {category}
+                            </div>
+                          </div>
+                          {activeSize === category && (
+                            <div className="border-1 w-7 border-black transition-transform duration-300"></div>
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                    <button
+                      className="underline cursor-pointer -translate-x-2.5 hover:text-gray-600 transition-colors"
+                      onClick={() => setIsSizeGuideOpen(true)}
+                    >
+                      size guide
+                    </button>
+                  </div>
+                </div>
+                <div className="mt-12.5 text-md ">Metal: </div>
+                <div className="h-10"></div>
+                <div className="mt-2.5 text-md font-medium">Band: </div>
+                <div className="h-10"></div>
               </div>
-              <div className="mt-12.5 text-md ">Metal: </div>
-              <div className="h-10"></div>
-              <div className="mt-2.5 text-md font-medium">Band: </div>
-              <div className="h-10"></div>
             </div>
 
             <div className="">
