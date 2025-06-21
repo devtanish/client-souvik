fc3a0e3750c8bf2f7c0bb163793f5764213aa460
profTanish <profile.tanish@gmail.com>
2025-06-21 21:10:32 +0530

    updated gemstone change pop-up hight

--- components/gemstone-explorer-tab.tsx
+++ components/gemstone-explorer-tab.tsx
@@ -154,7 +154,7 @@ export function GemstoneExplorerTab({
   return (
     <TooltipProvider>
       <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center p-4">
-        <div className="bg-white max-w-6xl w-full max-h-[90vh] overflow-hidden">
+        <div className="bg-white max-w-6xl w-full max-h-[100vh] overflow-hidden">
           {/* Header */}
           <div className="flex items-center justify-between p-6 border-b">
             <div>
