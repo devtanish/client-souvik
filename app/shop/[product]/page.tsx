3ecdaa355f981fc649980dede6c660a98096288e
profTanish <profile.tanish@gmail.com>
2025-07-12 01:48:33 +0530

    updated sticky product page

--- app/shop/[product]/page.tsx
+++ app/shop/[product]/page.tsx
@@ -5,7 +5,8 @@ export default function Home({ params }: { params: Promise<{ product: string }> 
   const { product } = React.use(params)
   return (
     <>
-      <ProductPage params={Promise.resolve({ product: product })}/>
+      <div className="w-screen h-25 bg-white z-10 top-7 md:flex hidden fixed"></div>
+      <ProductPage params={Promise.resolve({ product: product })} />
     </>
   )
 }
