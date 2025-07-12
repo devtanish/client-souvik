782462309c2d481270637c2cbf3a61f4e5c3314e
profTanish <profile.tanish@gmail.com>
2025-07-12 14:30:07 +0530

    reverse updated file

--- app/shop/[product]/page.tsx
+++ app/shop/[product]/page.tsx
@@ -5,7 +5,6 @@ export default function Home({ params }: { params: Promise<{ product: string }> 
   const { product } = React.use(params)
   return (
     <>
-      <div className="w-screen h-25 bg-white z-10 top-7 md:flex hidden fixed"></div>
       <ProductPage params={Promise.resolve({ product: product })} />
     </>
   )
