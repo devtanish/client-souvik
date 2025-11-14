7508e565eeb81097cdc77c7ceb6096b3c84fcdd6
devtanish <hellotanish2000@gmail.com>
2025-11-15 02:17:44 +0530

    updated footer issues

--- components/footer.tsx
+++ components/footer.tsx
@@ -86,7 +86,7 @@ export default function Footer() {
             />
             <Button className="w-full md:w-auto bg-transparent border border-white text-white rounded-none px-8 py-2 hover:bg-white hover:text-black transition-colors">
               JOIN NOW 
-              <div className="-translate-y-[0.17rem]">
+              <div className="-translate-y-[0.1rem]">
                 →
               </div>
             </Button>
@@ -164,7 +164,7 @@ export default function Footer() {
                     expandedSections.includes("Service") ? "max-h-[500px] opacity-100 mt-3" : "max-h-0 opacity-0"
                   }`}
                 >
-                  <div className="ml-5 mr-1 grid grid-cols-2 md:grid-cols-3 gap-6 text-sm text-gray-700">
+                  <div className="ml-3 mr-1.5 grid grid-cols-1 md:grid-cols-3 gap-6 text-sm text-gray-700">
                     {serviceLinks.map((items, key) => (
                       <li key={key} className={`${borderLog(key, serviceLinks.length) ? "md:border-b" : "md:border-0"} ${key === serviceLinks.length-1 ? "border-0" : "border-b"} pb-2 flex justify-between`}>
                         <Link href={items.href} className="text-gray-700 hover:underline">
@@ -194,7 +194,7 @@ export default function Footer() {
                     expandedSections.includes("Company") ? "max-h-[500px] opacity-100 mt-3" : "max-h-0 opacity-0"
                   }`}
                 >
-                  <div className="ml-5 mr-1 grid grid-cols-2 md:grid-cols-3 gap-6 text-sm text-gray-700">
+                  <div className="ml-3 mr-1.5 grid grid-cols-1 md:grid-cols-3 gap-6 text-sm text-gray-700">
                     {companyLinks.map((items, key) => (
                       <li key={key} className={`${borderLog(key, companyLinks.length) ? "md:border-b" : "md:border-0"} ${key === companyLinks.length-1 ? "border-0" : "border-b"} pb-2 flex justify-between`}>
                         <Link href={items.href} className="text-gray-700 hover:underline">
